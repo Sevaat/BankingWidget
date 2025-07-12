79b6dc5670b663fbd27f2653a0987192cf0377e9
tkachenko <sevaatmail@mail.com>
2025-07-12 17:55:52 +0500

    Исправлены имена

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,7 +1,7 @@
 import src.generators as gn
 
 
-def test_filter_by_state_executed(transactions, transactions_usd, transactions_rub):
+def test_filter_by_currency(transactions, transactions_usd, transactions_rub):
     assert list(gn.filter_by_currency(transactions, currency="USD")) == transactions_usd
     assert list(gn.filter_by_currency(transactions, currency="RUB")) == transactions_rub
     assert list(gn.filter_by_currency(transactions, currency="000")) == []
