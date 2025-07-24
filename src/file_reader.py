6d59ce6be0623701cb8ae38d4ec5d9a36fa9e44f
tkachenko <sevaatmail@mail.com>
2025-07-24 19:28:21 +0700

    Читать excel файлы
    функция чтения excel файлов для получения данных транзакций,
    тесты для функции.

--- src/file_reader.py
+++ src/file_reader.py
@@ -13,3 +13,17 @@ def csv_reader(filename: str = "") -> list[dict]:
         return transactions
     except Exception:
         return []
+
+
+def excel_reader(filename: str = "") -> list[dict]:
+    """
+    Функция получения списка транзакций из файла excel
+    :param filename: путь к файлу
+    :return: список транзакций
+    """
+    try:
+        reader = pd.read_excel(filename)
+        transactions = reader.to_dict(orient="records")
+        return transactions
+    except Exception:
+        return []
--- tests/test_file_reader.py
+++ tests/test_file_reader.py
@@ -21,3 +21,21 @@ def test_csv_reader():
     with patch("pandas.read_csv", side_effect=FileNotFoundError):
         result = fr.csv_reader("nonexistent.csv")
     assert result == []
+
+
+def test_excel_reader():
+    # корректное чтение
+    test_data = pandas.DataFrame([{"a": 1, "b": 2}, {"a": 3, "b": 4}])
+    with patch("pandas.read_excel", return_value=test_data):
+        result = fr.excel_reader("valid.xlsx")
+        assert result == [{"a": 1, "b": 2}, {"a": 3, "b": 4}]
+
+    # ошибки парсинга excel-файла
+    with patch("pandas.read_excel", side_effect=pandas.errors.EmptyDataError()):
+        result = fr.excel_reader("invalid.xlsx")
+    assert result == []
+
+    # проверка на отсутствие файла
+    with patch("pandas.read_excel", side_effect=FileNotFoundError):
+        result = fr.excel_reader("nonexistent.xlsx")
+    assert result == []
